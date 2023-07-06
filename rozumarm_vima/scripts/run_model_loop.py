b628fb6f4c7f82e5c0e14af101f989534252c3fb
fic-laptop <>
2023-07-06 16:21:46 +0300

    Updated script for real-obs pipeline.

--- rozumarm_vima/scripts/run_model_loop.py
+++ rozumarm_vima/scripts/run_model_loop.py
@@ -16,27 +16,72 @@ from rozumarm_vima.vima_model import VimaModel
 import argparse
 
 
+USE_OBS_FROM_SIM = True
 N_SWEPT_OBJECTS = 2
+USE_REAL_ROBOT = True
+# USE_FIXED_PROMPT_FOR_SIM = False
 
 
-def run_loop(r, robot, oracle, cubes_detector, model=None, n_iters=3):
+def run_loop(r, robot, oracle, model=None, n_iters=1):
     """
     r: scene renderer
     """
+    if USE_OBS_FROM_SIM:
+        from rozumarm_vima.detectors import detector
+
+        cubes_detector = detector
+        model.reset(r.env.prompt, r.env.prompt_assets)
+    else:
+        cam_1 = CamDenseReader(0, 'cam_top_video.mp4')
+        cam_2 = CamDenseReader(2, 'cam_front_video.mp4')
+        cam_1.start_recording()
+        cam_2.start_recording()
+
+        prompt_assets = get_prompt_assets()
+        prompt = r.env.prompt
+        # prompt = 'Sweep all /{swept_obj/} into /{bounds/} without exceeding /{constraint/}'
+        # print(prompt)
+        model.reset(prompt, prompt_assets)
+
     while True:
         for i in range(n_iters):
-            n_cubes = -1
-            while n_cubes != 2 * N_SWEPT_OBJECTS:
-                obj_posquats = cubes_detector.detect()
-                n_cubes = len(obj_posquats)
+            if USE_OBS_FROM_SIM:
+                n_cubes = -1
+                while n_cubes != 2 * N_SWEPT_OBJECTS:
+                    obj_posquats = cubes_detector.detect()
+                    n_cubes = len(obj_posquats)
 
-            # map from cam to rozum
-            obj_posquats = [
-                (rf_tf_c2r(pos), map_tf_repr_c2r(quat)) for pos, quat in obj_posquats
-            ]
+                # map from cam to rozum
+                obj_posquats = [
+                    (rf_tf_c2r(pos), map_tf_repr_c2r(quat)) for pos, quat in obj_posquats
+                ]
 
-            front_img, top_img = r.render_scene(obj_posquats)
-            obs, *_ = r.env.step(action=None)
+                front_img, top_img = r.render_scene(obj_posquats)
+                obs, *_ = r.env.step(action=None)
+            else:
+                _, image_top = cam_1.read_image()
+                _, image_front = cam_2.read_image()
+
+                segm_top, _ = segment_scene(image_top, "top")
+                segm_front, _ = segment_scene(image_front, "front")
+
+                img_top = cv2.resize(image_top, (256, 128))
+                img_front = cv2.resize(image_front, (256, 128))
+
+                img_top = cv2.rotate(img_top, cv2.ROTATE_180)
+                segm_top = cv2.rotate(segm_top, cv2.ROTATE_180)
+
+                obs = {
+                    'rgb': {
+                        'front': np.transpose(img_front, axes=(2, 0, 1)),
+                        'top': np.transpose(img_top, axes=(2, 0, 1))
+                    },
+                    'segm':{
+                        'front': segm_front,
+                        'top': segm_top
+                    },
+                    'ee': 1  # spatula
+                }
 
             meta_info = {'action_bounds':{'low': np.array([ 0.25, -0.5 ]), 'high': np.array([0.75, 0.5 ])}}
             
@@ -47,6 +92,12 @@ def run_loop(r, robot, oracle, cubes_detector, model=None, n_iters=3):
                                             7: {'obj_name': 'small block'}}
             
             action = model.step(obs, meta_info)
+            # with open(r'student_{}.dat'.format(model.elapsed_steps),'w+b') as file: 
+            #     obs['action'] = action
+            #     obs['prompt'] = r.env.prompt
+            #     obs['assets'] = r.env.prompt_assets
+            #     obs['inf_cache'] = model.inference_cache
+            #     pickle.dump(obs,file)
             # action = oracle.act(obs)
 
             if action is None:
@@ -87,101 +138,109 @@ def run_loop(r, robot, oracle, cubes_detector, model=None, n_iters=3):
             return
         if len(ret) > 0 and ret[0] == 'n':
             r.reset(exact_num_swept_objects=N_SWEPT_OBJECTS)
-            model.reset(r.env.prompt,r.env.prompt_assets)
+            prompt = r.env.prompt
+            # prompt = 'Sweep all /{swept_obj/} into /{bounds/} without exceeding /{constraint/}'
+            # print(prompt)
+            model.reset(prompt, r.env.prompt_assets)
         continue
+    
+    if USE_OBS_FROM_SIM:
+        detector.release()
 
 
-def run_loop_sim_to_real(r, prompt_assets, robot, oracle, model=None, n_iters=3):
-    """
-    r: scene renderer
-    """
+# def run_loop_sim_to_real(r, prompt_assets, robot, oracle, model=None, n_iters=3):
+#     """
+#     r: scene renderer
+#     """
 
-    cam_1 = CamDenseReader(3, 'cam_top_video.mp4')
-    cam_2 = CamDenseReader(2, 'cam_front_video.mp4')
-    cam_1.start_recording()
-    cam_2.start_recording()
-    sleep(3)
+#     cam_1 = CamDenseReader(0, 'cam_top_video.mp4')
+#     cam_2 = CamDenseReader(4, 'cam_front_video.mp4')
+#     cam_1.start_recording()
+#     cam_2.start_recording()
+#     sleep(3)
 
-    counter = 1
-    while True:
-        counter += 1
-        for i in range(n_iters):
-            _, image_top = cam_1.read_image()
-            _, image_front = cam_2.read_image()
+#     counter = 1
+#     while True:
+#         counter += 1
+#         for i in range(n_iters):
+#             _, image_top = cam_1.read_image()
+#             _, image_front = cam_2.read_image()
 
-            segm_top, _ = segment_scene(image_top, "top")
-            segm_front, _ = segment_scene(image_front, "front")
+#             segm_top, _ = segment_scene(image_top, "top")
+#             segm_front, _ = segment_scene(image_front, "front")
 
-            img_top = cv2.resize(image_top, (256, 128))
-            img_front = cv2.resize(image_front, (256, 128))
+#             img_top = cv2.resize(image_top, (256, 128))
+#             img_front = cv2.resize(image_front, (256, 128))
 
-            img_top = cv2.rotate(img_top, cv2.ROTATE_180)
-            segm_top = cv2.rotate(segm_top, cv2.ROTATE_180)
+#             img_top = cv2.rotate(img_top, cv2.ROTATE_180)
+#             segm_top = cv2.rotate(segm_top, cv2.ROTATE_180)
 
-            obs = {
-                'rgb': {
-                    'front': np.transpose(img_front, axes=(2, 0, 1)),
-                    'top': np.transpose(img_top, axes=(2, 0, 1))
-                },
-                'segm':{
-                    'front': segm_front,
-                    'top': segm_top
-                },
-                'ee': 1  # spatula
-            }
+#             obs = {
+#                 'rgb': {
+#                     'front': np.transpose(img_front, axes=(2, 0, 1)),
+#                     'top': np.transpose(img_top, axes=(2, 0, 1))
+#                 },
+#                 'segm':{
+#                     'front': segm_front,
+#                     'top': segm_top
+#                 },
+#                 'ee': 1  # spatula
+#             }
 
-            with open(f"model_input_{counter}.pickle", 'wb') as f:
-                pickle.dump({'obs': obs, 'prompt_assets': prompt_assets}, f, protocol=pickle.HIGHEST_PROTOCOL)
+#             with open(f"model_input_{counter}.pickle", 'wb') as f:
+#                 pickle.dump({'obs': obs, 'prompt_assets': prompt_assets}, f, protocol=pickle.HIGHEST_PROTOCOL)
 
-            meta_info = {'action_bounds':{'low': np.array([ 0.25, -0.5 ]), 'high': np.array([0.75, 0.5 ])}}
+#             meta_info = {'action_bounds':{'low': np.array([ 0.25, -0.5 ]), 'high': np.array([0.75, 0.5 ])}}
             
-            meta_info["n_objects"] = 4
-            meta_info["obj_id_to_info"] = {4: {'obj_name': 'three-sided rectangle'},
-                                            5: {'obj_name': 'line'},
-                                            6: {'obj_name': 'small block'},
-                                            7: {'obj_name': 'small block'}}
-            model.reset(r.env.prompt, prompt_assets)
-            action = model.step(obs,meta_info)
-            #action = oracle.act(obs)
-            if action is None:
-                print("Press Enter to try again, or q + Enter to exit.")
-                ret = input()
-                if len(ret) > 0 and ret[0] == 'q':
-                    cam_1.stop_recording()
-                    cam_2.stop_recording()
-                    return
-                r.reset(exact_num_swept_objects=1)
-                continue
-                # print("ORACLE FAILED.")
-                # # cubes_detector.release()
-                # return
+#             meta_info["n_objects"] = 4
+#             meta_info["obj_id_to_info"] = {4: {'obj_name': 'three-sided rectangle'},
+#                                             5: {'obj_name': 'line'},
+#                                             6: {'obj_name': 'small block'},
+#                                             7: {'obj_name': 'small block'}}
+#             model.reset(r.env.prompt, prompt_assets)
 
-            clipped_action = {
-                k: np.clip(v, r.env.action_space[k].low, r.env.action_space[k].high)
-                for k, v in action.items()
-            }
+#             # action = model.step(obs,meta_info)
+#             action = oracle.act(obs)
 
-            pos_0 = clipped_action["pose0_position"]
-            pos_1 = clipped_action["pose1_position"]
-            eef_quat = robot.get_swipe_quat(pos_0, pos_1)
+#             if action is None:
+#                 print("Press Enter to try again, or q + Enter to exit.")
+#                 ret = input()
+#                 if len(ret) > 0 and ret[0] == 'q':
+#                     cam_1.stop_recording()
+#                     cam_2.stop_recording()
+#                     return
+#                 r.reset(exact_num_swept_objects=1)
+#                 continue
+#                 # print("ORACLE FAILED.")
+#                 # # cubes_detector.release()
+#                 # return
+
+#             clipped_action = {
+#                 k: np.clip(v, r.env.action_space[k].low, r.env.action_space[k].high)
+#                 for k, v in action.items()
+#             }
+
+#             pos_0 = clipped_action["pose0_position"]
+#             pos_1 = clipped_action["pose1_position"]
+#             eef_quat = robot.get_swipe_quat(pos_0, pos_1)
             
-            x_compensation_bias = 0.03
-            pos_0[0] += x_compensation_bias
-            pos_1[0] += x_compensation_bias
+#             x_compensation_bias = 0.03
+#             pos_0[0] += x_compensation_bias
+#             pos_1[0] += x_compensation_bias
             
-            posquat_0 = (pos_0, eef_quat)
-            posquat_1 = (pos_1, eef_quat)
-            robot.swipe(posquat_0, posquat_1)
+#             posquat_0 = (pos_0, eef_quat)
+#             posquat_1 = (pos_1, eef_quat)
+#             robot.swipe(posquat_0, posquat_1)
 
-        print("Press Enter to start over...")
-        ret = input()
-        if len(ret) > 0 and ret[0] == 'q':
-            cam_1.stop_recording()
-            cam_2.stop_recording()
-            return
-        r.reset(exact_num_swept_objects=1)
-        model.reset(r.env.prompt,r.env.prompt_assets)
-        continue
+#         print("Press Enter to start over...")
+#         ret = input()
+#         if len(ret) > 0 and ret[0] == 'q':
+#             cam_1.stop_recording()
+#             cam_2.stop_recording()
+#             return
+#         r.reset(exact_num_swept_objects=1)
+#         model.reset(r.env.prompt,r.env.prompt_assets)
+#         continue
 
 
 def get_prompt_assets():
@@ -243,29 +302,21 @@ def main():
 
     r = VIMASceneRenderer('sweep_without_exceeding')
     oracle = r.env.task.oracle(r.env)
-    robot = RozumArm(use_mock_api=False)
+    robot = RozumArm(use_mock_api=not USE_REAL_ROBOT)
 
     arg = argparse.ArgumentParser()
     arg.add_argument("--partition", type=str, default="placement_generalization")
     arg.add_argument("--task", type=str, default="visual_manipulation")
     arg.add_argument("--ckpt", type=str, required=True)
     arg.add_argument("--device", default="cpu")
-    arg = arg.parse_args("--ckpt ./2M.ckpt --device cuda --task sweep_without_exceeding".split())    
+    arg = arg.parse_args("--ckpt ./20M.ckpt --device cuda --task sweep_without_exceeding".split())    
 
     model = VimaModel(arg)
-    #model = RuDolphModel()
+    # model = RuDolphModel()
 
     r.reset(exact_num_swept_objects=N_SWEPT_OBJECTS)
 
-    # prompt_assets = get_prompt_assets()
-    # model.reset(r.env.prompt, prompt_assets)
-    model.reset(r.env.prompt, r.env.prompt_assets)
-    
-    from rozumarm_vima.detectors import detector
-
-    run_loop(r, robot, oracle, cubes_detector=detector, model=model, n_iters=1)
-    # run_loop_sim_to_real(r, prompt_assets, robot, oracle, model=model, n_iters=2)
-    detector.release()
+    run_loop(r, robot, oracle, model=model, n_iters=1)
 
 
 """
